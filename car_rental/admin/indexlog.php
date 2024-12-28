f3446ff68dbddb794692db8df2850a3e4775f284
shawon719 <sharmony.hashem2001@gmail.com>
2024-12-29 00:04:20 +0600

    add

--- car_rental/admin/indexlog.php
+++ car_rental/admin/indexlog.php
@@ -1,3 +1,100 @@
-<?php
-   echo  "this is admin login page.";
+
+<?php 
+        session_start();
+   
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="icon" type="image/png" href="./asstes/images/Pharmanest (1).png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./bootstrap/css/adminlte.min.css">
+    <style>
+        body{
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .form-container{
+            background-color: yellowgreen;
+            padding: 30px;
+            width: 100%;
+            max-width: 450px;
+            height: 400px;
+            border-radius: 10px;
+        }
+
+        /* Styling for the close button */
+.close-btn {
+    position: relative;
+    /* top: 5px;  */
+    left: 370px; 
+    
+    /* padding-left: 380px; */
+    /* padding-top: 1px; */
+    background: none;
+    border: none;
+    font-size: 30px;
+    cursor: pointer;
+    color: #555;
+}
+
+.close-btn:hover {
+    color: #f00;
+    border: 1px solid white;
+}
+    </style>
+   
+</head>
+<body>
+    <!-- this is login page.which text file is index  .php -->
+    <div class="form-container">
+            
+        <form action="" method="post">
+        <div class="">
+                 <button class="close-btn" onclick="closeForm()"><b>×</b></button>
+            </div>
+
+        
+            <div class="text-center">
+                <h3>Log In</h3>
+            </div>
+            
+
+            <!-- email section -->
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" name="txtEmail" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+
+            <!-- password section -->
+             <div class="mb-3">
+                <label for="inputPassword5" class="form-label">Password</label>
+                    <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+                    <!-- <div id="passwordHelpBlock" class="form-text">
+                        Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                     </div> -->
+
+             </div>
+
+             <div class="d-grid gap-2">
+                <button class="btn btn-primary" type="button" name="btn">Button</button>
+                <!-- <button class="btn btn-primary" type="button">Button</button> -->
+            </div>
+            
+
+
+             </div>
+        </form>
+    </div>
+
+
+    <script src="./bootstrap/js/adminlte.min.js"></script>
+   
+</body>
+</html>
