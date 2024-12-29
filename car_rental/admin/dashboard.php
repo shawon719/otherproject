5952bb6c149b73a5b461aae7110d68e7f25f0736
shawon719 <sharmony.hashem2001@gmail.com>
2024-12-29 12:58:32 +0600

    add

--- car_rental/admin/dashboard.php
+++ car_rental/admin/dashboard.php
@@ -16,8 +16,21 @@
     <link rel="stylesheet" href="./bootstrap/fontawesome/css/fontawesome.min.css">
     <!-- <link rel="stylesheet" href="css/font-awesome.min.css"> -->
 </head>
-<body>
-    <h1>This is my dashboard</h1>
+
+
+        <body class="layout-fixed sidebar-expand-lg sidebar-mini bg-body-tertiary"> <!--begin::App Wrapper-->
+    <div class="app-wrapper"> <!--begin::Header-->
+        <?php 
+         //include_once "include/nav.php";
+        ?>
+        <!--end::Header--> 
+        <!--begin::Sidebar-->
+        <?php 
+         //include_once "include/sidebar.php";
+        ?>
+        <!--end::Sidebar--> <!--begin::App Main-->
+
+    <!-- <h1>This is my dashboard</h1> -->
     <?php
          include("./includes/sidebar.php");
     ?>
--- car_rental/admin/includes/sidebar.php
+++ car_rental/admin/includes/sidebar.php
@@ -1,5 +1,5 @@
 <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
-            <div class="sidebar-brand"> <!--begin::Brand Link--> <a href="../index.html" class="brand-link"> <!--begin::Brand Image--> <img src="./asstes/images/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image opacity-75 shadow"> <!--end::Brand Image--> <!--begin::Brand Text--> <span class="brand-text fw-light">AdminLTE 4</span> <!--end::Brand Text--> </a> <!--end::Brand Link--> </div> <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
+            <div class="sidebar-brand"> <!--begin::Brand Link--> <a href="../index.html" class="brand-link"> <!--begin::Brand Image--> <img src="./img/logo.svg" alt="no image" class="brand-image opacity-75 shadow"> <!--end::Brand Image--> <!--begin::Brand Text--> <span class="brand-text fw-light"><b>EuropaCar</b></span> <!--end::Brand Text--> </a> <!--end::Brand Link--> </div> <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
             <div class="sidebar-wrapper">
                 <nav class="mt-2"> <!--begin::Sidebar Menu-->
                     <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
