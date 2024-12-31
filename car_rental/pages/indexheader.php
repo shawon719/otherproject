45d213e26961a3f6afc42e88628a9f1e9c051789
shawon719 <sharmony.hashem2001@gmail.com>
2025-01-01 00:35:51 +0600

    add

---
+++ car_rental/pages/indexheader.php
@@ -0,0 +1,43 @@
+<?php
+    echo "this will be index header";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>index page</title>
+    <link rel="stylesheet" href="./bootstrap//dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+     <link rel="stylesheet" href="./bootstrap/fontawesome/css/all.min.css">
+     <link rel="stylesheet" href="./bootstrap/css/adminlte.min.css">
+    
+     <script src="./bootstrap/fontawesome/js/all.min.js"></script>
+     <title>indexheader</title>
+     
+</head>
+<body>
+    <!-- navbar start -->
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container">
+                   <div header="header_component_right">
+                      <a href="#">
+                        <div>
+                          <img src="./bootstrap/img/main-logo.569c225.svg" alt="europa">
+                        </div>
+                      </a>
+
+                   </div>
+           
+          </div>
+    </nav>
+  <!-- navbar end -->
+
+
+      
+    <script src="./bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="./bootstrap/js/adminlte.min.js"></script>
+</body>
+</html>
