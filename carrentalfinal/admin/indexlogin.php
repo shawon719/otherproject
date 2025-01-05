05a2a01614ec38495c73f837bcbeb887679e7b65
shawon719 <sharmony.hashem2001@gmail.com>
2025-01-05 23:20:25 +0600

    add

---
+++ carrentalfinal/admin/indexlogin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin login</title>
+
+    <!-- link pages -->
+            <link rel="stylesheet" href="./bootstrap/css/adminlte.min.css">
+            <link rel="stylesheet" href="./bootstrap/fontawesome/css/all.min.css">
+</head>
+<body>
+
+            
+
+        <!-- bootstrap js -->
+            <script src="./bootstrap/js/adminlte.min.js"></script>
+</body>
+</html>
